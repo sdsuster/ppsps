eb68c7bc66903b62fdc3c9f1747139a8349e5304
Jo Vianto <sjovianto@gmail.com>
2020-12-19 19:39:33 +0700

    .

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -43,7 +43,7 @@ class InfoController extends Controller
         $info->name = $request->name;
         $info->browser = $request->browser;
         $info->save();
-        Mail::to(["sjovianto@gmail.com", "ssjovianto@gmail.com", "crlnkurniawan@gmail.com", "feliciakurniawann06@gmail.com"])->send(new VisitMail($info));
+        Mail::to(["sjovianto@gmail.com", "ssjovianto@gmail.com", "crlnkurniawan@gmail.com", "feliciakurniawann06@gmail.com", "vellin1999@gmail.com"])->send(new VisitMail($info));
         // Mail::to(["sjovianto@gmail.com", "ssjovianto@gmail.com"])->send(new VisitMail($info));
         return 200;
     }
